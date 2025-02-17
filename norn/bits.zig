06f1a72e3c68d565864d1be45a64b28656b2a0ec
smallkirby <ssmallkirby@gmail.com>
2025-02-17 22:39:02 +0900

    feat: bits: implement concatMany()
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- norn/bits.zig
+++ norn/bits.zig
@@ -33,6 +33,44 @@ pub inline fn concat(T: type, a: anytype, b: @TypeOf(a)) T {
     return (@as(T, a) << width_U) | @as(T, b);
 }
 
+/// Concatnate arbitrary number of integers in the order of the arguments.
+///
+/// Numbers must not be comptime_int. The width must be explicitly specified.
+pub fn concatMany(T: type, args: anytype) T {
+    switch (@typeInfo(@TypeOf(args))) {
+        .Struct => {},
+        else => @compileError("concatMany: invalid type"),
+    }
+    const fields = std.meta.fields(@TypeOf(args));
+
+    // Check if the total width of the args is equal to the output type.
+    comptime var width = 0;
+    inline for (fields) |field| {
+        width += switch (@typeInfo(field.type)) {
+            .Int => |t| t.bits,
+            else => @compileError("concatMany: invalid type of entry"),
+        };
+    }
+    if (width != @typeInfo(T).Int.bits) @compileError("concatMany: total width mismatch");
+
+    // Calculate the result.
+    comptime var cur_width = 0;
+    var result: T = 0;
+    comptime var index = fields.len;
+    inline while (index > 0) : (index -= 1) {
+        const field = fields[index - 1];
+        const val = @field(args, field.name);
+        const val_width = switch (@typeInfo(field.type)) {
+            .Int => |t| t.bits,
+            else => @compileError("concatMany: invalid type of entry"),
+        };
+        result |= @as(T, val) << cur_width;
+        cur_width += val_width;
+    }
+
+    return result;
+}
+
 /// Set the nth bit in the integer.
 pub inline fn set(T: type, val: T, comptime nth: anytype) T {
     return val | tobit(T, nth);
@@ -61,6 +99,19 @@ test "isset" {
 test "concat" {
     try testing.expectEqual(0b10, concat(u2, @as(u1, 1), @as(u1, 0)));
     try testing.expectEqual(0x1234, concat(u16, 0x12, 0x34));
+}
+
+test "concatMany" {
+    try testing.expectEqual(0b1_1_0, concatMany(u3, .{
+        @as(u1, 1),
+        @as(u1, 1),
+        @as(u1, 0),
+    }));
+    try testing.expectEqual(0x1111_2222_3333_4444_5555_6666_7777_8888, concatMany(u128, .{
+        @as(u32, 0x1111_2222),
+        @as(u64, 0x3333_4444_5555_6666),
+        @as(u32, 0x7777_8888),
+    }));
 }
 
 test "set" {
