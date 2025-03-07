1b1f48f8a418e482b03e3456e781812715d61d89
smallkirby <ssmallkirby@gmail.com>
2025-03-07 23:11:07 +0900

    fix: surtr: properly set EFER.NX
    Previous impl did NOT work at all.
    It didn't cause problem just because UEFI sets NX bit by itself.
    
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- surtr/arch/x86/arch.zig
+++ surtr/arch/x86/arch.zig
@@ -1,9 +1,11 @@
+const am = @import("asm.zig");
+
 pub const page = @import("page.zig");
 
 /// Enable NX-bit.
 pub fn enableNxBit() void {
-    const efer_reg: *volatile u64 = @ptrFromInt(0xC000_0080);
-    efer_reg.* = efer_reg.* | (1 << 11);
+    const efer = am.rdmsr(u64, 0xC000_0080);
+    am.wrmsr(0xC000_0080, efer | (1 << 11)); // EFER.NX
 }
 
 /// Halt the current CPU.
--- surtr/arch/x86/asm.zig
+++ surtr/arch/x86/asm.zig
@@ -23,3 +23,40 @@ pub inline fn flushTlbSingle(virt: u64) void {
         : "memory"
     );
 }
+
+pub fn rdmsr(T: type, comptime msr: u64) T {
+    var eax: u32 = undefined;
+    var edx: u32 = undefined;
+    asm volatile (
+        \\rdmsr
+        : [eax] "={eax}" (eax),
+          [edx] "={edx}" (edx),
+        : [msr] "{ecx}" (msr),
+        : "eax", "edx", "ecx"
+    );
+
+    const value = (@as(u64, edx) << 32) | eax;
+    return switch (@typeInfo(T)) {
+        .int, .comptime_int => value,
+        .@"struct" => @bitCast(value),
+        else => @compileError("rdmsr: invalid type"),
+    };
+}
+
+pub fn wrmsr(comptime msr: u64, value: anytype) void {
+    const val: u64 = switch (@typeInfo(@TypeOf(value))) {
+        .int, .comptime_int => value,
+        .@"struct" => @bitCast(value),
+        else => @compileError("wrmsr: invalid type"),
+    };
+    const eax: u32 = @truncate(val);
+    const edx: u32 = @truncate(val >> 32);
+    asm volatile (
+        \\wrmsr
+        :
+        : [msr] "{ecx}" (msr),
+          [eax] "{eax}" (eax),
+          [edx] "{edx}" (edx),
+        : "eax", "edx", "ecx"
+    );
+}
