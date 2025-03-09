e2e4f8aa3687beb2f7aca416d1f02991ef0e0fbc
smallkirby <ssmallkirby@gmail.com>
2025-03-09 17:07:00 +0900

    debug: update apps/init
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- apps/init/main.zig
+++ apps/init/main.zig
@@ -1,6 +1,16 @@
 export fn _start() callconv(.naked) noreturn {
     asm volatile (
-        \\.L1:
-        \\jmp .L1
+        \\movq $0x12345678, %%rdi
+        \\movq $0xDEADBEEF, %%rsi
+        \\movq $0x87654321, %%rdx
+        \\movq $0xCAFEBABE, %%r10
+        \\movq $0x11223344, %%r8
     );
+
+    while (true) {
+        asm volatile (
+            \\movq $0, %%rax
+            \\syscall
+        );
+    }
 }
