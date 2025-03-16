282ba252bde607e53d62a5dddb6da2f90dec82c0
smallkirby <ssmallkirby@gmail.com>
2025-03-16 21:26:36 +0900

    test: check output for some message indicating runtime test success
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- runtime_test.bash
+++ runtime_test.bash
@@ -9,6 +9,26 @@ CPU_FEATURES=fsgsbase
 TIMEOUT=60
 TMPFILE=$(mktemp)
 
+SUCCESS_HEYSTACKS=(
+  "Unhandled syscall (nr=18446744073709551615)"
+  "[2]=0000000000000001"
+  "[3]=0000000000000002 [4]=0000000000000003"
+)
+
+function check_success()
+{
+  ret=0
+
+  for needle in "${SUCCESS_HEYSTACKS[@]}"; do
+    if ! grep -qF "$needle" "$TMPFILE"; then
+      echo "[-] Missing: $needle"
+      ret=1
+    fi
+  done
+
+  return $ret
+}
+
 function cleanup()
 {
   rm -f "$TMPFILE"
@@ -46,6 +66,14 @@ if [ $((ret >> 1 << 1)) -ne 0 ]; then
   cleanup
   exit 1
 fi
-
 echo "[+] QEMU exited with code 0."
+
+echo "[+] Checking output..."
+if ! check_success; then
+  echo "[-] Output does not contain expected strings."
+  cleanup
+  exit 1
+fi
+echo "[+] All expected strings found."
+
 cleanup
