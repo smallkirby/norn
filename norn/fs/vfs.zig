ea7a7dca8abaa20588eb1c70ff4da052897f7ab5
smallkirby <ssmallkirby@gmail.com>
2025-03-22 11:02:40 +0900

    refactor: vfs: minor fix
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- norn/fs/vfs.zig
+++ norn/fs/vfs.zig
@@ -2,8 +2,10 @@
 pub const Error = error{
     /// Failed to allocate memory.
     OutOfMemory,
-    /// Invalid operation on the inode.
-    InvalidOperation,
+    /// Operation for directory only is called on a non-directory.
+    NotDirectory,
+    /// Operation for regular file only is called on a non-regular file.
+    IsDirectory,
 };
 
 /// Stat information.
@@ -38,36 +40,36 @@ pub const Dentry = struct {
 
     /// Dentry operations.
     pub const Vtable = struct {
-        /// Create a file named `name` in this directory inode.
-        ///
-        /// If the file is created successfully, return the dentry.
-        createFile: *const fn (self: *Dentry, name: []const u8) Error!*Dentry,
         /// Create a directory named `name` in this directory inode.
         ///
         /// If the directory is created successfully, return the dentry.
         createDirectory: *const fn (self: *Dentry, name: []const u8) Error!*Dentry,
+        /// Create a file named `name` in this directory inode.
+        ///
+        /// If the file is created successfully, return the dentry.
+        createFile: *const fn (self: *Dentry, name: []const u8) Error!*Dentry,
     };
-
-    /// Create a file named `name` in this directory inode.
-    pub fn createFile(self: *Self, name: []const u8) Error!*Dentry {
-        const inode = self.inode;
-        if (inode.inode_type != InodeType.directory) return Error.InvalidOperation;
-
-        return self.ops.createFile(self, name);
-    }
 
     /// Create a directory named `name` in this directory inode.
     pub fn createDirectory(self: *Self, name: []const u8) Error!*Dentry {
         const inode = self.inode;
-        if (inode.inode_type != InodeType.directory) return Error.InvalidOperation;
+        if (inode.inode_type != InodeType.directory) return Error.NotDirectory;
 
         return self.ops.createDirectory(self, name);
+    }
+
+    /// Create a file named `name` in this directory inode.
+    pub fn createFile(self: *Self, name: []const u8) Error!*Dentry {
+        const inode = self.inode;
+        if (inode.inode_type != InodeType.directory) return Error.NotDirectory;
+
+        return self.ops.createFile(self, name);
     }
 };
 
 /// Inode type.
 pub const InodeType = enum {
-    /// File
+    /// Regular file
     file,
     /// Directory
     directory,
@@ -95,22 +97,27 @@ pub const Inode = struct {
         /// If the file is found, return the dentry.
         /// If the file is not found, return null.
         lookup: *const fn (self: *Inode, name: []const u8) Error!?*Dentry,
-        /// TODO: doc
+        /// Read data from this inode from position `pos` to `buf`.
+        ///
+        /// Return the number of bytes read.
         read: *const fn (inode: *Inode, buf: []u8, pos: usize) Error!usize,
-        /// TODO: doc
+        /// Write data to this inode from position `pos` with `data`.
+        ///
+        /// Return the number of bytes written.
         write: *const fn (inode: *Inode, data: []const u8, pos: usize) Error!usize,
-        /// TODO: doc
+        /// Get stat information of this inode.
         stat: *const fn (inode: *Inode) Error!Stat,
     };
 
     /// Lookup a file named `name` in this directory inode.
     pub fn lookup(self: *Self, name: []const u8) Error!?*Dentry {
-        if (self.inode_type != InodeType.directory) return Error.InvalidOperation;
+        if (self.inode_type != InodeType.directory) return Error.NotDirectory;
         return self.ops.lookup(self, name);
     }
 
     /// Read data from this inode.
     pub fn read(self: *Self, buf: []u8, pos: usize) Error!usize {
+        if (self.inode_type == InodeType.directory) return Error.IsDirectory;
         return self.ops.read(self, buf, pos);
     }
 
@@ -121,9 +128,14 @@ pub const Inode = struct {
 
     /// Write data to this inode.
     pub fn write(self: *Self, data: []const u8, pos: usize) Error!usize {
+        if (self.inode_type == InodeType.directory) return Error.IsDirectory;
         return self.ops.write(self, data, pos);
     }
 };
+
+// =============================================================
+// Imports
+// =============================================================
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
