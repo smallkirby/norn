f8d66977ba187c08b4c79c5f01980d63efcf14e0
smallkirby <ssmallkirby@gmail.com>
2025-03-23 23:15:31 +0900

    feat: util: add pointer comparison utility
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- norn/util.zig
+++ norn/util.zig
@@ -10,6 +10,26 @@ pub inline fn rounddown(value: usize, alignment: usize) usize {
     return value & ~(alignment - 1);
 }
 
+/// Returns true if the lhs pointer is larger than or equal to the rhs pointer.
+pub inline fn ptrGte(lhs: anytype, rhs: @TypeOf(lhs)) bool {
+    return @intFromPtr(lhs) >= @intFromPtr(rhs);
+}
+
+/// Returns true if the lhs pointer is larger than the rhs pointer.
+pub inline fn ptrGt(lhs: anytype, rhs: @TypeOf(lhs)) bool {
+    return @intFromPtr(lhs) > @intFromPtr(rhs);
+}
+
+/// Returns true if the lhs pointer is less than or equal to the rhs pointer.
+pub fn ptrLte(lhs: anytype, rhs: @TypeOf(lhs)) bool {
+    return @intFromPtr(lhs) <= @intFromPtr(rhs);
+}
+
+/// Returns true if the lhs pointer is less than the rhs pointer.
+pub fn ptrLt(lhs: anytype, rhs: @TypeOf(lhs)) bool {
+    return @intFromPtr(lhs) < @intFromPtr(rhs);
+}
+
 // =======================================
 
 const testing = std.testing;
@@ -36,6 +56,41 @@ test "rounddown" {
     try testing.expectEqual(0x1000, rounddown(0x1FFF, 0x1000));
 }
 
-// =======================================
+test "ptrGte" {
+    var a: u32 = 5;
+    var b: u32 = 3;
+    try testing.expectEqual(false, ptrGte(&a, &b));
+    try testing.expectEqual(true, ptrGte(&a, &a));
+    try testing.expectEqual(true, ptrGte(&b, &a));
+}
+
+test "ptrGt" {
+    var a: u32 = 5;
+    var b: u32 = 3;
+    try testing.expectEqual(false, ptrGt(&a, &b));
+    try testing.expectEqual(false, ptrGt(&a, &a));
+    try testing.expectEqual(true, ptrGt(&b, &a));
+}
+
+test "ptrLte" {
+    var a: u32 = 5;
+    var b: u32 = 3;
+    try testing.expectEqual(true, ptrLte(&a, &b));
+    try testing.expectEqual(true, ptrLte(&a, &a));
+    try testing.expectEqual(false, ptrLte(&b, &a));
+}
+
+test "ptrLt" {
+    var a: u32 = 5;
+    var b: u32 = 3;
+    try testing.expectEqual(true, ptrLt(&a, &b));
+    try testing.expectEqual(false, ptrLt(&a, &a));
+    try testing.expectEqual(false, ptrLt(&b, &a));
+}
+
+// =============================================================
+// Imports
+// =============================================================
 
 const std = @import("std");
+const norn = @import("norn");
