cdc65789f28d5fe3cbaa11ca815ff7370411b7f5
smallkirby <ssmallkirby@gmail.com>
2025-04-05 13:32:01 +0900

    refactor: more convenient StackCreator
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- norn/thread.zig
+++ norn/thread.zig
@@ -186,113 +186,315 @@ pub fn createInitialThread(comptime filename: []const u8) Error!*Thread {
 
     // Initialize user stack.
     var sc = StackCreator.new(stack_vma) catch @panic("StackCreator.new");
-    const argv0 = sc.pushData(filename); // argv[0] data
-    _ = sc.push(@as(u64, 0)); // padding for alignment
-    _ = sc.push(@as(u64, 0)); // end marker
-    _ = sc.push(@as(u128, 0x0123_4567_89AB_CDEF_1122_3344_5566_7788)); // Random value for AT_RANDOM
-    _ = sc.push(AuxVector.new(.terminator, 0)); // AT_NULL (auxvec)
-    _ = sc.push(AuxVector.new(.random, stack_base + stack_size - 0x20)); // AT_RANDOM (auxvec)
-    _ = sc.push(@as(u64, 0)); // NULL (envp)
-    _ = sc.push(@as(u64, 0)); // NULL (argv)
-    _ = sc.push(@as(u64, @intFromPtr(argv0))); // NULL (argv[0])
-    _ = sc.push(@as(u64, 1)); // argc
+    try sc.appendArgvs(&.{ filename, "sh" });
+    const at_random_handle = try sc.appendOpaqueData(u128, 0x0123_4567_89AB_CDEF_1122_3344_5566_7788);
+    try sc.appendAuxvWithHandle(AuxVector.new(.random, at_random_handle));
+    const stack_top = try sc.finalize();
 
     // Set up user stack.
     arch.task.setupUserContext(
         thread,
         elf_loader.entry_point,
-        stack_base + stack_size - sc.size(),
+        stack_top,
     );
 
     return thread;
 }
 
 /// Construct stack frame in the given area.
+///
+/// You build a stack by calling these APIS.
+/// Until you call `finalize()`, the stack is not touched.
 const StackCreator = struct {
     const Self = @This();
 
-    /// Upper limit of the stack.
-    _stack_top: [*]u8,
-    /// Bottom of the stack.
-    _stack_bottom: [*]u8,
-    /// Stack pointer.
-    _sp: [*]u8,
-    /// User stack VMA.
-    _user_vma: *const Vma,
+    /// User stack.
+    _stack: Stack,
+    /// argv
+    _argvs: ArgvList,
+    /// envp
+    _envps: EnvpList,
+    /// auxv with immediate value.
+    _imm_auxvs: AuxVectorList,
+    /// auxv with opaque data.
+    _handle_auxvs: AuxVectorList,
+    /// Opaque data.
+    _opaque_data: OpaqueList,
+
+    /// argv type.
+    const Argv = []const u8;
+    /// argv list type.
+    const ArgvList = ArrayList(Argv);
+    /// envp type.
+    const Envp = []const u8;
+    /// envp list type.
+    const EnvpList = ArrayList(Envp);
+    /// auxv list type.
+    const AuxVectorList = ArrayList(AuxVector);
+    /// Opaque data type.
+    const OpaqueType = struct {
+        data: []const u8,
+        handle: StackOpaqueHandler,
+        pointer: u64 = undefined,
+    };
+    /// Opaque data list type.
+    const OpaqueList = ArrayList(OpaqueType);
+
+    /// Stack alignment in bytes.
+    const alignment = 16;
 
     /// Instantiate a new stack creator.
     ///
     /// Cursor is set to the bottom of the stack.
     pub fn new(stack_vma: *const Vma) !StackCreator {
-        const start = stack_vma.start;
-        const stack_size = stack_vma.end - stack_vma.start;
-        if (start % 0x10 != 0) return error.InvalidStack;
-        if (stack_size % 0x10 != 0) return error.InvalidStack;
-
-        const slice = stack_vma.slice();
-
         return .{
-            ._stack_top = @constCast(@ptrCast(slice.ptr)),
-            ._stack_bottom = @ptrFromInt(@intFromPtr(slice.ptr) + slice.len),
-            ._sp = @ptrFromInt(@intFromPtr(slice.ptr) + slice.len),
-            ._user_vma = stack_vma,
+            ._stack = try Stack.new(stack_vma),
+            ._argvs = ArgvList.init(general_allocator),
+            ._envps = EnvpList.init(general_allocator),
+            ._imm_auxvs = AuxVectorList.init(general_allocator),
+            ._handle_auxvs = AuxVectorList.init(general_allocator),
+            ._opaque_data = OpaqueList.init(general_allocator),
         };
     }
 
-    /// Push a value to the stack.
+    /// Append an argv.
     ///
-    /// The size of value must be 8-byte aligned.
-    /// Returns the address of the pushed value.
-    /// Note that the address belongs to user, and you MUST NOT touch it.
-    pub fn push(self: *Self, value: anytype) [*]const u8 {
-        const T = @TypeOf(value);
-
-        const value_size = @sizeOf(T);
-        comptime norn.comptimeAssert(
-            value_size != 0,
-            "StackCreator does not support zero-sized type",
-        );
-        comptime norn.comptimeAssert(
-            value_size % 8 == 0,
-            "StackCreator only supports 64-bit aligned types",
-        );
-
-        if (util.ptrLte(self._sp - value_size, self._stack_top)) {
-            @panic("StackCreator overflow");
-        }
-        self._sp -= value_size;
-
-        const ptr: *T = @alignCast(@ptrCast(self._sp));
-        ptr.* = value;
-
-        const diff = @intFromPtr(self._sp) - @intFromPtr(self._stack_top);
-        return @ptrFromInt(self._user_vma.start + diff);
+    /// The argument `argv` must not be freed until you call `finalize()`.
+    pub fn appendArgv(self: *Self, argv: []const u8) !void {
+        try self._argvs.append(argv);
     }
 
-    /// Push an u8 array to the stack.
+    /// Append argvs.
     ///
-    /// The address of the data is aligned to 8 bytes.
-    /// Returns the address of the pushed value.
-    /// Note that the address belongs to user, and you MUST NOT touch it.
-    pub fn pushData(self: *Self, comptime value: []const u8) [*]const u8 {
-        const aligned_size = util.roundup(value.len, 8);
-        if (util.ptrLte(self._sp - aligned_size, self._stack_top)) {
-            @panic("StackCreator overflow");
+    /// The argument `argvs` must not be freed until you call `finalize()`.
+    pub fn appendArgvs(self: *Self, argvs: []const []const u8) !void {
+        try self._argvs.appendSlice(argvs);
+    }
+
+    /// Append an envp.
+    ///
+    /// The argument `envp` must not be freed until you call `finalize()`.
+    pub fn appendEnvp(self: *Self, envp: []const u8) !void {
+        try self._envps.append(envp);
+    }
+
+    /// Append envps.
+    ///
+    /// The argument `envps` must not be freed until you call `finalize()`.
+    pub fn appendEnvps(self: *Self, envps: []const []const u8) !void {
+        try self._envps.appendSlice(envps);
+    }
+
+    /// Append an auxv with an immediate value.
+    pub fn appendAuxvImmediate(self: *Self, auxv: AuxVector) !void {
+        try self._imm_auxvs.append(auxv);
+    }
+
+    /// Append an auxv with a handle to opaque data.
+    pub fn appendAuxvWithHandle(self: *Self, auxv: AuxVector) !void {
+        try self._handle_auxvs.append(auxv);
+    }
+
+    /// Append an arbitrary opaque data.
+    ///
+    /// Returns the handler of the data.
+    /// You can pass the handler as a data, that's resolve to the pointer to the opaque data.
+    pub fn appendOpaqueData(self: *Self, T: type, data: T) !StackOpaqueHandler {
+        const duped = try general_allocator.create(T);
+        duped.* = data;
+        const raw_ptr: [*]const u8 = @alignCast(@ptrCast(duped));
+        const u8_size = @sizeOf(T);
+
+        const handle: StackOpaqueHandler = self._opaque_data.items.len;
+        try self._opaque_data.append(.{ .data = raw_ptr[0..u8_size], .handle = handle });
+        return handle;
+    }
+
+    /// Perform the construction of the user stack frame.
+    ///
+    /// Calling this function constructs the user stack frame.
+    /// You can free the arguments passed to this function after calling this function.
+    ///
+    /// Returns the address of the top of the user stack.
+    pub fn finalize(self: *Self) !u64 {
+        // Push opaque data.
+        var opaque_num_pushed: usize = 0;
+        while (opaque_num_pushed < self._opaque_data.items.len) : (opaque_num_pushed += 1) {
+            const data = self._opaque_data.items[opaque_num_pushed];
+            const addr = self._stack.pushData(data.data);
+            self._opaque_data.items[opaque_num_pushed].pointer = @intFromPtr(addr);
         }
-        self._sp -= aligned_size;
-        @memcpy(self._sp[0..value.len], value);
 
-        const diff = @intFromPtr(self._sp) - @intFromPtr(self._stack_top);
-        return @ptrFromInt(self._user_vma.start + diff);
+        // Push envp strings.
+        var envp_addrs = ArrayList(u64).init(general_allocator);
+        defer envp_addrs.deinit();
+
+        var envp_num_pushed: usize = 0;
+        while (envp_num_pushed < self._envps.items.len) : (envp_num_pushed += 1) {
+            const envp = self._envps.items[self._envps.items.len - envp_num_pushed - 1];
+            const addr = self._stack.pushData(envp);
+            try envp_addrs.append(@intFromPtr(addr));
+        }
+
+        // Push argv strings.
+        var argv_addrs = ArrayList(u64).init(general_allocator);
+        defer argv_addrs.deinit();
+
+        var argv_num_pushed: usize = 0;
+        while (argv_num_pushed < self._argvs.items.len) : (argv_num_pushed += 1) {
+            const argv = self._argvs.items[self._argvs.items.len - argv_num_pushed - 1];
+            const addr = self._stack.pushData(argv);
+            try argv_addrs.append(@intFromPtr(addr));
+        }
+
+        // Adjust alignment to 16 bytes.
+        self._stack.makeAlignment();
+
+        // Construct auxv array.
+        _ = self._stack.push(AuxVector.new(.terminator, 0)); // AT_NULL
+        var auxv_num_pushed: usize = 0;
+        while (auxv_num_pushed < self._imm_auxvs.items.len) : (auxv_num_pushed += 1) {
+            const auxv: AuxVector = self._imm_auxvs.items[auxv_num_pushed];
+            _ = self._stack.push(auxv);
+        }
+        auxv_num_pushed = 0;
+        while (auxv_num_pushed < self._handle_auxvs.items.len) : (auxv_num_pushed += 1) {
+            const auxv: AuxVector = self._handle_auxvs.items[auxv_num_pushed];
+            const opaque_ptr = self._opaque_data.items[auxv.value].pointer;
+            _ = self._stack.push(AuxVector.new(auxv.auxv_type, opaque_ptr));
+        }
+
+        // Construct envp array.
+        envp_num_pushed = 0;
+        _ = self._stack.push(@as(u64, 0)); // NULL (envp)
+        while (envp_num_pushed < self._envps.items.len) : (envp_num_pushed += 1) {
+            _ = self._stack.push(@as(u64, envp_addrs.items[envp_num_pushed]));
+        }
+
+        // Construct argv array.
+        argv_num_pushed = 0;
+        _ = self._stack.push(@as(u64, 0)); // NULL (argv)
+        while (argv_num_pushed < self._argvs.items.len) : (argv_num_pushed += 1) {
+            _ = self._stack.push(@as(u64, argv_addrs.items[argv_num_pushed]));
+        }
+
+        // Push argc.
+        _ = self._stack.push(@as(u64, argv_num_pushed));
+
+        // Free items.
+        for (self._opaque_data.items) |data| {
+            general_allocator.free(data.data);
+        }
+        self._opaque_data.deinit();
+        self._handle_auxvs.deinit();
+        self._imm_auxvs.deinit();
+        self._envps.deinit();
+        self._argvs.deinit();
+
+        return self._stack.getUserStackTop();
     }
 
-    /// Get the total size of the pushed values on the stack.
-    pub fn size(self: *Self) usize {
-        return self._stack_bottom - self._sp;
-    }
+    /// User stack mapper.
+    const Stack = struct {
+        /// Upper limit of the stack.
+        _stack_top: [*]u8,
+        /// Bottom of the stack.
+        _stack_bottom: [*]u8,
+        /// Stack pointer.
+        _sp: [*]u8,
+
+        /// User stack VMA.
+        _user_vma: *const Vma,
+
+        /// Instantiate a new stack information from the userland VMA.
+        pub fn new(stack_vma: *const Vma) !Stack {
+            const start = stack_vma.start;
+            const stack_size = stack_vma.end - stack_vma.start;
+            if (start % alignment != 0) return error.InvalidStack;
+            if (stack_size % alignment != 0) return error.InvalidStack;
+
+            const slice = stack_vma.slice();
+
+            return .{
+                ._stack_top = @constCast(@ptrCast(slice.ptr)),
+                ._stack_bottom = @ptrFromInt(@intFromPtr(slice.ptr) + slice.len),
+                ._sp = @ptrFromInt(@intFromPtr(slice.ptr) + slice.len),
+                ._user_vma = stack_vma,
+            };
+        }
+
+        /// Push a value to the stack.
+        ///
+        /// The size of value must be 8-byte aligned.
+        /// Returns the address of the pushed value.
+        /// Note that the address belongs to user, and you MUST NOT touch it.
+        fn push(self: *Stack, value: anytype) [*]const u8 {
+            const T = @TypeOf(value);
+
+            const value_size = @sizeOf(T);
+            comptime norn.comptimeAssert(
+                value_size != 0,
+                "StackCreator does not support zero-sized type",
+            );
+            comptime norn.comptimeAssert(
+                value_size % 8 == 0,
+                "StackCreator only supports 64-bit aligned types",
+            );
+
+            if (util.ptrLte(self._sp - value_size, self._stack_top)) {
+                @panic("StackCreator overflow");
+            }
+            self._sp -= value_size;
+
+            const ptr: *T = @alignCast(@ptrCast(self._sp));
+            ptr.* = value;
+
+            const diff = @intFromPtr(self._sp) - @intFromPtr(self._stack_top);
+            return @ptrFromInt(self._user_vma.start + diff);
+        }
+
+        /// Push an u8 array to the stack.
+        ///
+        /// The address of the pushed data is aligned to 8 bytes.
+        /// Returns the address of the pushed value.
+        ///
+        /// Note that the address belongs to user, and you MUST NOT touch it.
+        fn pushData(self: *Stack, value: []const u8) [*]const u8 {
+            const aligned_size = util.roundup(value.len, alignment);
+            if (util.ptrLte(self._sp - aligned_size, self._stack_top)) {
+                @panic("StackCreator overflow");
+            }
+            self._sp -= aligned_size;
+            @memcpy(self._sp[0..value.len], value);
+
+            const diff = @intFromPtr(self._sp) - @intFromPtr(self._stack_top);
+            return @ptrFromInt(self._user_vma.start + diff);
+        }
+
+        /// Align the stack pointer to the alignment.
+        fn makeAlignment(self: *Stack) void {
+            const diff = @intFromPtr(self._sp) % alignment;
+            if (diff != 0) {
+                self._sp -= diff;
+            }
+        }
+
+        /// Get the total size of the pushed values on the stack.
+        fn size(self: *Stack) usize {
+            return self._stack_bottom - self._sp;
+        }
+
+        /// Get the top of the user stack.
+        fn getUserStackTop(self: *Stack) u64 {
+            return self._user_vma.end - self.size();
+        }
+    };
+
+    /// Opaque data that can be resolved to a pointer to the actual data on user stack.
+    const StackOpaqueHandler = u64;
 };
 
-/// Auxiliary vector for passing information to the program.
+/// Auxiliary vector for passing information from Norn to the user process.
 const AuxVector = packed struct(u128) {
     /// Type of the entry.
     auxv_type: AuxvType,
@@ -331,6 +533,7 @@ const AuxVector = packed struct(u128) {
         sysinfo_ehdr = 33,
     };
 
+    /// Construct an auxiliary vector using immediate value.
     pub fn new(auxv_type: AuxvType, value: u64) AuxVector {
         return .{
             .auxv_type = auxv_type,
@@ -344,6 +547,7 @@ const AuxVector = packed struct(u128) {
 // =============================================================
 
 const std = @import("std");
+const ArrayList = std.ArrayList;
 
 const norn = @import("norn");
 const arch = norn.arch;
